d8d241fe08ed3f54cc44df10e428cdebc3019d61
Stéphanie <stef-x06@hotmail.fr>
2020-02-09 17:45:43 +0100

    implement level 3

---
+++ level-3/router.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+
+const {addTrip, tripsList, deleteTrip, getTripById, updateTrip} = require('./service');
+
+/**
+ * [GET] /trips
+ * Return all the trips in a list
+ */
+router.get('/trips', function(req, res) {
+  if (req.headers.authorization === "Basic secret") {
+    console.log("condition true");
+    res.send(tripsList());
+    res.status(200);
+    return;
+  }
+
+  res.status(403);
+  res.send('You are not allowed to see this');
+});
+
+/**
+* [GET] /trips/:id
+* Return the trip requested
+*/
+router.get('/trips/:id', function(req, res) {
+  if (req.headers.authorization === "Basic secret") {
+    console.log("condition true");
+    res.send(getTripById(req.params.id));
+    res.status(200);
+    return;
+  }
+
+  res.status(403);
+  res.send('You are not allowed to see this');
+});
+
+/**
+* [POST] /trips
+* Create a new trip
+*/
+router.post('/trips', function(req, res) {
+  addTrip(req.body);
+  res.send('Trip added');
+});
+
+/**
+* [POST] /trips/:id
+* Update a trip
+*/
+router.post('/trips/:id', function(req, res) {
+  updateTrip(req.params.id, req.body);
+  res.send('Trip updated');
+});
+
+/**
+* [DELETE] /trips/:id
+* Delete a trip
+*/
+router.delete('/trips/:id', function(req, res) {
+  deleteTrip(req.params.id);
+  res.send('Trip deleted');
+});
+
+module.exports = router;
---
+++ level-3/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const router = require('./router');
+
+app.use(bodyParser.json());
+app.use(router);
+
+app.listen(8080);
