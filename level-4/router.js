d2eccaa4b31ea697eb388b6f792b829ae6e4e438
Stéphanie <stef-x06@hotmail.fr>
2020-02-09 20:25:36 +0100

    remove middleware function + move to arrow functions

--- level-4/router.js
+++ level-4/router.js
@@ -1,28 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const {addTrip, tripsList, deleteTrip, getTripById, updateTrip} = require('./service');
+const {authMiddleware} = require('./middleware');
 
-router.use(function (req, res, next) {
-  if (req.method === 'GET') {
-    if (req.headers.authorization === "Basic secret") {
-      next();
-      return;
-    }
-
-    res.status(403);
-    res.send('You are not allowed to see this');
-
-    return;
-  }
-
-  next();
+router.use((req, res, next) => {
+  authMiddleware(req, res, next);
 });
 
 /**
  * [GET] /trips
  * Return all the trips in a list
  */
-router.get('/trips', function(req, res) {
+router.get('/trips', (req, res) => {
   res.send(tripsList());
   res.status(200);
 });
@@ -31,7 +20,7 @@ router.get('/trips', function(req, res) {
 * [GET] /trips/:id
 * Return the trip requested
 */
-router.get('/trips/:id', function(req, res) {
+router.get('/trips/:id', (req, res) => {
   res.send(getTripById(req.params.id));
   res.status(200);
 });
@@ -40,7 +29,7 @@ router.get('/trips/:id', function(req, res) {
 * [POST] /trips
 * Create a new trip
 */
-router.post('/trips', function(req, res) {
+router.post('/trips', (req, res) => {
   addTrip(req.body);
   res.send('Trip added');
 });
@@ -49,7 +38,7 @@ router.post('/trips', function(req, res) {
 * [POST] /trips/:id
 * Update a trip
 */
-router.post('/trips/:id', function(req, res) {
+router.post('/trips/:id', (req, res) => {
   updateTrip(req.params.id, req.body);
   res.send('Trip updated');
 });
@@ -58,7 +47,7 @@ router.post('/trips/:id', function(req, res) {
 * [DELETE] /trips/:id
 * Delete a trip
 */
-router.delete('/trips/:id', function(req, res) {
+router.delete('/trips/:id', (req, res) => {
   deleteTrip(req.params.id);
   res.send('Trip deleted');
 });
