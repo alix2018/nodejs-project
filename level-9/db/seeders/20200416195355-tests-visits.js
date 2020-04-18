7b1406bb171bdf2aa32dce0f485e82572ebe3b96
Stéphanie <stef-x06@hotmail.fr>
2020-04-18 16:29:45 +0200

    fix visits seeder

--- level-9/db/seeders/20200416195355-tests-visits.js
+++ level-9/db/seeders/20200416195355-tests-visits.js
@@ -2,13 +2,10 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    console.log("SEEDER VISITS", users);
-
-    const usersPromise = queryInterface.sequelize.query(`SELECT id, name from Users;`
+    const usersPromise = queryInterface.sequelize.query(`SELECT id, username from Users;`
     )
       .then(users => users[0].reduce((accumulator, item) => {
-          console.log("usersPromise");
-          accumulator[item.name] = item.id;
+          accumulator[item.username] = item.id;
           return accumulator;
         }, {})
       )
@@ -16,64 +13,60 @@ module.exports = {
     const countriesPromise = queryInterface.sequelize.query(`SELECT id, name from Countries;`
     )
       .then(countries => countries[0].reduce((accumulator, item) => {
-          console.log("countriesPromise");
           accumulator[item.name] = item.id;
           return accumulator;
         }, {})
       )
 
     return Promise.all([usersPromise, countriesPromise])
-      .then(([users, coutries]) => {
-        console.log("users:", users);
-        console.log("coutries:", coutries);
-
+      .then(([users, countries]) => {
         return queryInterface.bulkInsert('Visits', [
         {
           visit_date: new Date('2018', '09', '01'),
-          user_id: users[Stephanie],
-          country_id: countries[France],
+          user_id: users.Stephanie,
+          country_id: countries.France,
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           visit_date: new Date('2019', '10', '12'),
-          user_id: users[Stephanie],
-          country_id: countries[Belgium],
+          user_id: users.Stephanie,
+          country_id: countries.Belgium,
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           visit_date: new Date('2020', '02', '20'),
-          user_id: users[Stephanie],
-          country_id: countries[Netherlands],
+          user_id: users.Stephanie,
+          country_id: countries.Netherlands,
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           visit_date: new Date('2017', '05', '01'),
-          user_id: users[Antoine],
-          country_id: countries[Germany],
+          user_id: users.Antoine,
+          country_id: countries.Germany,
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           visit_date: new Date('2020', '02', '20'),
-          user_id: users[Antoine],
-          country_id: countries[Netherlands],
+          user_id: users.Antoine,
+          country_id: countries.Netherlands,
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           visit_date: new Date('2012', '07', '12'),
-          user_id: users[Charlie],
-          country_id: countries[France],
+          user_id: users.Charlie,
+          country_id: countries.France,
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           visit_date: new Date('2015', '03', '22'),
-          user_id: users[Charlie],
-          country_id: countries[Jacob],
+          user_id: users.Jacob,
+          country_id: countries['Czech Republic'],
           createdAt: new Date(),
           updatedAt: new Date()
         }
